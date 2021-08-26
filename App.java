d8318ecf4c1e9bb531a616335ff69df4f81e70b0
Igor Mamushin <imamushin@1worldsync.com>
2021-08-26 22:14:11 +0400

    Fixed solution

--- App.java
+++ App.java
@@ -1,28 +1,28 @@
-import java.util.ArrayList;
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
 import java.util.Locale;
 import java.util.Map;
-import java.util.Stack;
 import java.util.TreeMap;
 
 public class App {
 	static public class ValueNode implements INode {
-		private long value;
+		private int value;
 
-		public ValueNode(long value) {
+		public ValueNode(int value) {
 			this.value = value;
 		}
 
 		public TreeMap<Long, Float> Calculate() {
 			TreeMap<Long, Float> result = new TreeMap<Long, Float>();
-			result.put(value, 1.0f);
+			result.put((long)value, 1.0f);
 			return result;
 		}
 	}
 
 	static public class DiceNode implements INode {
-		private Long faceCount;
+		private int faceCount;
 
-		public DiceNode(Long faceCount) {
+		public DiceNode(int faceCount) {
 			this.faceCount = faceCount;
 		}
 
@@ -30,8 +30,8 @@ public class App {
 			TreeMap<Long, Float> result = new TreeMap<Long, Float>();
 			
 			float prohibitance = 1.0f / this.faceCount;
-			for(Long i = 1l; i <= faceCount; i++){
-				result.put(i, prohibitance);
+			for(int i = 1; i <= faceCount; i++){
+				result.put((long)i, prohibitance);
 			}
 
 			return result;
@@ -43,13 +43,9 @@ public class App {
 		private char operator;
 		private INode leftNode;
 		private INode rightNode;
-		private int priority;
 
-		public OperatorNode(
-			char operator,
-			int priority) {
+		public OperatorNode(char operator) {
 			this.operator = operator;
-			this.priority = priority;
 		}
 
 		public void SetLeftNode(INode leftNode) {
@@ -60,9 +56,6 @@ public class App {
 			this.rightNode = rightNode;
 		}
 
-		public int GetPriority(){
-			return this.priority;
-		}
 		
 		public TreeMap<Long, Float> Calculate() {
 			TreeMap<Long, Float> leftResult = this.leftNode.Calculate();
@@ -103,122 +96,141 @@ public class App {
 		public TreeMap<Long, Float> Calculate();
 	}
 
-	static public class TreeExpression {
-		private ArrayList<INode> expressionList = new ArrayList<INode>();
-		private Stack<OperatorNode> lastOperations = new Stack<OperatorNode>();
-		private OperatorNode header;
-		
-		public TreeMap<Long, Float> Calculate() {
-			return header.Calculate();
-		}
-
-		public void AddToTree(String strNumber, char operation, boolean isDice, int priority) {
-			Long number = (long)Integer.parseInt(strNumber);
-			INode node = null;
-			if (isDice) {
-				node = new DiceNode(number);
-			} else {
-				node = new ValueNode(number);
-			}
-			expressionList.add(node);
-			OperatorNode operator = new OperatorNode(operation, priority);
-
-			if (!lastOperations.empty()) {
-				var lastOperator = lastOperations.peek();
-
-				if (lastOperator.GetPriority() > operator.GetPriority()) {
-					expressionList.add(lastOperations.pop()); 
-				} 
-			}
-
-			lastOperations.push(operator);
-		}
-
-		public void AddLastValue(String strNumber, boolean isDice) {
-			Long number = (long)Integer.parseInt(strNumber);
-			INode node = null;
-			if (isDice) {
-				node = new DiceNode(number);
-				isDice = false;
-			} else {
-				node = new ValueNode(number);
-			}
-
-			expressionList.add(node);
-
-			while (!lastOperations.empty()) {
-				expressionList.add(lastOperations.pop());
-			}
-
-			int i = 2;
-			while (i < expressionList.size()) {
-				var opeartor = (expressionList.get(i) instanceof OperatorNode ? (OperatorNode)expressionList.get(i) : null);
-
-				if (opeartor != null){
-					opeartor.SetLeftNode(expressionList.get(i-2));
-					opeartor.SetRightNode(expressionList.get(i-1));
-
-					expressionList.remove(i-1);
-					expressionList.remove(i-2);
-					i--;
-				} else {
-					i++;
-				}
-			}
-
-			header = (OperatorNode)expressionList.get(0);
-		}
-	}
-
 	public static void main(String[] args) {
 		java.util.Scanner in = new java.util.Scanner(System.in);
 		String line = in.nextLine();
-		TreeExpression expression = ParseExpression(line);
-		TreeMap<Long, Float> result = expression.Calculate();
+		INode node = ParseExpression(line);
+		TreeMap<Long, Float> result = node.Calculate();
 
 		for (Map.Entry<Long, Float> m : result.entrySet()) {
 			System.out.println(m.getKey() + " " + String.format(Locale.US, "%.2f", (float)Math.round(m.getValue()*100*100)/100));
 		}
 	}
 
-    private static TreeExpression ParseExpression(String expresion){
-		var arrExpression = expresion.toCharArray();
-		var treeExpression = new TreeExpression();
+    private static INode ParseExpression(String expresion){
+		CharacterIterator it = new StringCharacterIterator(expresion);
 
-		String strNumber = "";
-		boolean isDice = false;
-		int priority = 0;
+		return CompareExpr(it);
+	}
 
-		for(int i = 0; i < arrExpression.length; i++){
-			char c = arrExpression[i];
+	private static INode CompareExpr(CharacterIterator it){
+		if (GetToken(it) == CharacterIterator.DONE){
+			return null;
+		}
 
-			if (Character.isDigit(c)) {
-				strNumber += arrExpression[i];
-			} else if (c == '+' || c == '-') {
-				treeExpression.AddToTree(strNumber, c, isDice, priority + 3);
-				strNumber = "";
-				isDice = false;
-			} else if (c == '*') {
-				treeExpression.AddToTree(strNumber, c, isDice, priority + 5);
-				strNumber = "";
-				isDice = false;
-			} else if (c == '>') {
-				treeExpression.AddToTree(strNumber, c, isDice, priority);
-				strNumber = "";
-				isDice = false;
-			} else if (c == ')') {
-				priority -= 10;
-				isDice = false;
-			} else if (c == '(') {
-				priority += 10;
-				isDice = false;
-			} else if (c == 'd') {
-				isDice = true;
+		INode left = PlusMinusExpr(it);
+		while (GetToken(it) != CharacterIterator.DONE){
+			if (GetToken(it) == '>') {
+				it.next();
+
+				var operation = new OperatorNode('>');
+				INode right = PlusMinusExpr(it);
+				operation.SetLeftNode(left);
+				operation.SetRightNode(right);
+				left = operation;
+			} else {
+				return left;
 			}
 		}
 
-		treeExpression.AddLastValue(strNumber, isDice);
+		return left;
+	}
 
-		return treeExpression;
+	private static INode PlusMinusExpr(CharacterIterator it){
+		if (GetToken(it) == CharacterIterator.DONE){
+			return null;
+		}
+
+		INode left = MultipleExpr(it);
+		while (GetToken(it) != CharacterIterator.DONE){
+			if (GetToken(it) == '+' || GetToken(it) == '-') {
+				var c = GetToken(it);
+				it.next();
+
+				var operation = new OperatorNode(c);
+				INode right = MultipleExpr(it);
+				operation.SetLeftNode(left);
+				operation.SetRightNode(right);
+				left = operation;
+			} else {
+				return left;
+			}
+		}
+
+		return left;
+	}
+
+	private static INode MultipleExpr(CharacterIterator it) {
+		if (GetToken(it) == CharacterIterator.DONE){
+			return null;
+		}
+
+		INode left = PrimaryValue(it);
+
+		while (GetToken(it) != CharacterIterator.DONE){
+			if (GetToken(it) == '*') {
+				var c = GetToken(it);
+				it.next();
+	
+				var operation = new OperatorNode(c);
+				INode right = PrimaryValue(it);
+				operation.SetLeftNode(left);
+				operation.SetRightNode(right);
+				left = operation;
+			}
+			else{
+				return left;
+			}
+		}
+
+		return left;
+	}
+
+	private static INode PrimaryValue(CharacterIterator it) {
+		if (GetToken(it) == CharacterIterator.DONE){
+			return null;
+		}
+
+		if (Character.isDigit(GetToken(it))) {
+			int number = GetNumber(it);
+			return new ValueNode(number);
+		} 
+		
+		if (it.current() == 'd') {
+			it.next();
+			int number = GetNumber(it);
+			return new DiceNode(number);
+		} 
+		
+		if (GetToken(it) == '(') {
+			it.next();
+			var node = CompareExpr(it);
+			if (GetToken(it) == ')'){
+				it.next();
+			}
+
+			return node;
+		}
+
+		return null;
+	}
+
+	private static int GetNumber(CharacterIterator it){
+		String strNumber = "";
+		while (Character.isDigit(it.current())){
+			strNumber += it.current();
+			it.next();
+		}
+
+		return Integer.parseInt(strNumber);
+	}
+
+	private static char GetToken(CharacterIterator it) {
+		if (it.current() == ' '){
+			it.next();
+			return GetToken(it);
+		}
+
+		return it.current();
 	}
 }
